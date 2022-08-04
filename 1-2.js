f82a25fe36282b6083ab8d688ab483d94ed20af6
Stas2936 <stascalko@mail.ru>
2022-08-05 00:24:55 +0300

    first task

---
+++ 1-2.js
@@ -0,0 +1,3 @@
+console.log("3" * true);
+console.log('5' * 5);
+console.log(10 * true);
---
+++ 1-4.js
@@ -0,0 +1,6 @@
+let num = 123;
+console.log( String(num) );
+let str = 'Hello';
+console.log( Boolean(str) );
+let bool = false;
+console.log( Number(bool) );
